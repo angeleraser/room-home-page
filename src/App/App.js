7362c5807e795592cccba5a57b991ae81356c435
Angel Figuera <angel.figuera@osanasalud.com>
2020-12-26 19:11:40 -0400

    feat: add slider context to manage products slider images

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import Header from "../components/Header/Header";
+export const SliderContext = createContext();
 export const App = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const providerValue = {
+    currentImageIndex,
+    setCurrentImageIndex,
+  };
   return (
-    <>
+    <SliderContext.Provider value={providerValue}>
       <Header />
-    </>
+    </SliderContext.Provider>
   );
 };
